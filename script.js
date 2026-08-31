80bb388dade32f1edd9faaa4f6113c7478975eea
JohnMwanza <JohnMwanza268@gmail.com>
2026-08-31 13:24:50 +0200

    fixed some bugs

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice () {
     let compChoice = Math.floor(Math.random() * 3) + 1;
     
@@ -6,14 +10,45 @@ function getComputerChoice () {
     else return "scissors"
 }
 
-function getHumanChoice (choice) {
+function getHumanChoice () {
+let choice = prompt("Enter a number between 1 and 3: ");
+
+    choice = Number(choice)
+
     if (choice === 1) {
         return "rock"
     }
     else if (choice === 2) {
         return "paper"
     }
-    else return "scissors"
+    else if (choice === 3) return "scissors"
 }
 
+function playRound(humanChoice,computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
 
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    } else {
+        computerScore++;
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+    
+}
+
+function playGame () {
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection,computerSelection)
+
+}
+
+playGame()
